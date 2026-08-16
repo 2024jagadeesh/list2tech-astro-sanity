2fcf5a5dd1067a7f0893d1b8cdec92aab898d569
2024jagadeesh <gsjagadeesh2024@gmail.com>
2026-08-16 21:50:31 +0530

    Configure Astro SSR for Render

--- list2tech-frontend/astro.config.mjs
+++ list2tech-frontend/astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
+  server: {
+    host: true,
+  },
 });
